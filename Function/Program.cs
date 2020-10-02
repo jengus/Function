49b61feb7133c2b217dcaeac5ca942bf2a98fda2
Evgeniy <zhenua2002.8@mail.ru>
2020-10-02 16:58:05 +0500

    Prog

---
+++ Function/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Function
+{
+    class Program
+    {
+        static double sqr (double x)
+        {
+            return (x*x);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите минимальное значение");
+            double min = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите максимальное значение");
+            double max = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите шаг построения");
+            double step = double.Parse(Console.ReadLine());
+
+            for (double i = min; i <= max; i+=step)
+            {
+                Console.WriteLine($"{Math.Round(i, 6)} {Math.Round(sqr(i), 6)}");
+            }
+        }
+    }
+}
